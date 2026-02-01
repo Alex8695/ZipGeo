411e60e30083bedd1b13d930bcfc0b36856bedf5
Alex8695 <39283168+Alex8695@users.noreply.github.com>
2026-02-01 14:24:24 -0500

    Add files via upload

---
+++ src/data/Zip_Geo.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  1 13:42:44 2026
+
+@author: alex
+"""
+
+class ZipGeo:
+    
+    def __importData(self):
+        dataLines:list
+
+        with open("data/zipGeoData.txt") as file:
+            dataLines = file.readlines()
+            
+        self._dataMaster.clear()
+
+        for line in dataLines[1:]:
+            temp = line.split("|")
+            self._dataMaster[temp[0]] = [float(temp[6].strip()),float(temp[7].strip())]
+        
+        pass
+
+    def __init__(self):
+        self._dataMaster = {}
+        self.__importData()
+        pass
+
+
+    def Find(self, zipcode:str):
+        """
+        Find lattitude and longitude by ZIP code\n
+        ZipGeo.Find("48823")
+
+        Parameters
+        ----------
+        zipcode : str
+            ZIP Code being searched.
+
+        Returns
+        -------
+        dict
+            zip, lat, lon.
+
+        """
+
+        q = self._dataMaster[zipcode]
+        return {"zip":zipcode,"lat":self._dataMaster[zipcode][0],"lon":self._dataMaster[zipcode][1]}
+    
+    
+if __name__ == "__main__":
+    zg = ZipGeo()
+    print(zg.Find("48823"))
